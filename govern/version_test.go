9bfc7d68c53ab0ab6378aea83322f4d02fe9cb7e
Barry Allen <future.speeder@gmail.com>
2019-09-10 23:10:03 +0800

    add version compare testing

--- govern/version_test.go
+++ govern/version_test.go
@@ -30,3 +30,17 @@ func TestVersion_String(t *testing.T) {
 	v := govern.Version{5, 35, 1451, govern.Release}
 	assert.Equal(t, v.String(), "v5.35.1451-release")
 }
+
+func TestVersionCompare(t *testing.T) {
+	v1 := govern.Version{1, 3, 41, govern.NoLabel}
+	v2 := govern.Version{1, 3, 40, govern.NoLabel}
+	v3 := govern.Version{2, 1, 34, govern.NoLabel}
+	v4 := govern.Version{2, 5, 1, govern.NoLabel}
+	v5 := govern.Version{2, 5, 1, govern.Release}
+	v6 := govern.Version{2, 5, 1, govern.Gamma}
+	v7 := govern.Version{2, 5, 1, govern.Gamma}
+	assert.Equal(t, v1.GreaterThan(v2), true)
+	assert.Equal(t, v3.LessThan(v4), true)
+	assert.Equal(t, v6.LessOrEqualThan(v5), true)
+	assert.Equal(t, v7.GreaterOrEqualThan(v6), true)
+}
